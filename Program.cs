fbff0c09f818d33b38fbe270057f4d4c453f30f6
hyrumjackson <hyrumjjackson@gmail.com>
2024-01-30 16:14:49 -0700

    Starting files

--- Program.cs
+++ Program.cs
@@ -1,7 +1,47 @@
-﻿internal class Program
-{
-    private static void Main(string[] args)
-    {
-        Console.WriteLine("Hello, World!");
-    }
-}
+﻿using TicTacToe_IS02_09;
+
+Tools t = new Tools();
+
+
+//Team 1:
+//Welcome the user to the game
+
+//Create a game board array to store the players’ choices
+
+//Ask each player in turn for their choice and update the game board array
+
+//Print the board by calling the method in the supporting class
+
+//Check for a winner by calling the method in the supporting class,
+//and notify the players when a win has occurred and which player won the game
+
+
+//Team 2:
+//Receive the “board” array from the driver class
+//Contain a method that prints the board based on the information passed to it
+//
+//  public static void PrintBoard (string[] boardArray)
+//
+//Contain a method that receives the game board array as input and returns if there is a winner and who it was
+//
+//  public static string DeclareWinner (string[] boardArray)
+
+//
+
+//Print Format
+//
+//  x | o | x
+// ---+---+---
+//  o | x | o
+// ---+---+---
+//  x | o | x
+//
+//Array Format
+//
+//  1 | 2 | 3
+// ---+---+---
+//  4 | 5 | 6
+// ---+---+---
+//  7 | 8 | 9
+//
+// [1, 2, 3, 4, 5, 6, 7, 8, 9]
---
+++ Tools.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TicTacToe_IS02_09
+{
+    internal class Tools
+    {
+
+        //Team 2:
+        //Receive the “board” array from the driver class
+        //Contain a method that prints the board based on the information passed to it
+        //
+        //  public static void PrintBoard (string[] boardArray)
+        //
+        //Contain a method that receives the game board array as input and returns if there is a winner and who it was
+        //
+        //  public static string DeclareWinner (string[] boardArray)
+
+        //
+
+        //Print Format
+        //
+        //  x | o | x
+        // ---+---+---
+        //  o | x | o
+        // ---+---+---
+        //  x | o | x
+        //
+        //Array Format
+        //
+        //  1 | 2 | 3
+        // ---+---+---
+        //  4 | 5 | 6
+        // ---+---+---
+        //  7 | 8 | 9
+        //
+        // [1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+    }
+}
